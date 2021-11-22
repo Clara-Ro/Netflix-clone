fa5a0be353d07d38ef14d573135fb9297a7a3aa4
Clara-Ro <lara.rhee.gree@gmail.com>
2021-11-22 11:09:56 -0300

    banner feito

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -52,5 +52,25 @@ export default{
             } 
         ]
        
+    },
+    getMovieInfo: async(movieId, type) => {
+        let info = {}
+
+        if(movieId){
+            switch(type){
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${Api_key}`)
+                break;
+
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${Api_key}`)
+                break;
+
+                default:
+                    info = null
+                break;
+            }
+        }
+        return info
     }
 }
