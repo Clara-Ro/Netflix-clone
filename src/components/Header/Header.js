07dd7d5cca5e0ec3b81cbdc8f9c7d1b102135360
Clara-Ro <lara.rhee.gree@gmail.com>
2021-11-22 13:05:47 -0300

    Header feito

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import './Header.css';
 
-export default()=>{
+export default({black})=>{
     return(
-        <header>
-            ....
+        <header className={black ? 'black' : ''}>
+            <div className='header--logo'>
+                <a href='/'>
+                    <img src = 'https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png' alt='netflix'/>
+                </a> 
+            </div>
+            <div className='header--user'>
+            <a href='/'>
+                    <img src = 'https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png' alt='usuario'/>
+                </a>
+            </div>
         </header>
     )
 }
